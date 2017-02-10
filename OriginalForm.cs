6cd31d25fbb22d95d50720fd0049e7a3d2b48653
Bernhard Punz <me@inreb.info>
2017-02-10 15:39:52 +0100

    red cursor, resizing mirror to 1260x800

--- OriginalForm.cs
+++ OriginalForm.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -49,7 +50,7 @@ namespace OEAMTCMirror
         private HotKey HK;
         private bool _clicked = false;
 
-        
+
 
         [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
         protected override void WndProc(ref Message m)
@@ -261,7 +262,13 @@ namespace OEAMTCMirror
                     {
                         r = new Rectangle(cursorX, cursorY, cursorBMP.Width, cursorBMP.Height);
                         g = Graphics.FromImage(desktopBMP);
-                        g.DrawImage(cursorBMP, r);
+
+
+                        Bitmap Dummy = new Bitmap(Properties.Resources.cursor_red);
+
+                        //g.DrawImage(cursorBMP, r);
+                        g.DrawImage(Dummy, r);
+
                         g.Flush();
 
                         GC.Collect();
@@ -338,6 +345,8 @@ namespace OEAMTCMirror
             {
                 Bitmap bmp = CaptureApplication(_mirrorState.SelectedProcess);
 
+                bmp = FixedSize(bmp, 1260, 800);
+
                 string stamp = DateTime.Now.ToString("hh.mm.ss.ffffff");
 
                 //this.DrawToBitmap(bmp, new Rectangle(Point.Empty, bmp.Size));
@@ -382,6 +391,49 @@ namespace OEAMTCMirror
                 MessageBox.Show(ex.ToString());
             }
         }
+
+        static Bitmap FixedSize(Image imgPhoto, int Width, int Height)
+        {
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+            int destX = 0;
+            int destY = 0;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)Width / (float)sourceWidth);
+            nPercentH = ((float)Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+            {
+                nPercent = nPercentH;
+                destX = Convert.ToInt16((Width - (sourceWidth * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentW;
+                destY = Convert.ToInt16((Height - (sourceHeight * nPercent)) / 2);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(Width, Height, PixelFormat.Format24bppRgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.Clear(Color.White);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto, new Rectangle(destX, destY, destWidth, destHeight), new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight), GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+
 
         private Process GetProcessFromActiveWindow()
         {
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -81,6 +81,16 @@ namespace OEAMTCMirror.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap cursor_red {
+            get {
+                object obj = ResourceManager.GetObject("cursor_red", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon icon_active {
